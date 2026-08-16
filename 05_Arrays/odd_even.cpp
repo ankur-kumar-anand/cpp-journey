30817886326b5ece84078b158c2ddc64b5fea6db
Ankur Kumar Anand <ankur.anand2k1@gmail.com>
2026-08-16 23:34:31 +0530

    adding vector solutions

--- 05_Arrays/odd_even.cpp
+++ 05_Arrays/odd_even.cpp
@@ -1,5 +1,6 @@
 //Q.Multiply even numbers of an array by 2 and square the odd numbers
-#include<iostream>
+
+/*#include<iostream>
 #include<vector>
 using namespace std;
 int main(){
@@ -11,4 +12,19 @@ int main(){
      for(int elem:v){     //for_each loop
         cout<<elem<<" "; //straight forward traversal
     }
+}*/
+
+//if we are using for_each loop: the output won't change unless -
+// By adding '&' to elem will use reference to modify it and hence now, it will work.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v={4,3,8,2,9};
+    for(int &elem:v){  //adding & to elem
+        if(elem%2==0) elem=elem*2;
+        else elem=elem*elem;
+    }
+    for(int &elem:v) cout<<elem<<" ";  //adding & to elem
 }
