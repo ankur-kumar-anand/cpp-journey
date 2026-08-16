3c99bfbe72c3f8b20c015551be2de8dbc787d67f
Ankur Kumar Anand <ankur.anand2k1@gmail.com>
2026-08-16 23:43:03 +0530

    adding vector solutions

--- 05_Arrays/Builtin_functionalities.cpp
+++ 05_Arrays/Builtin_functionalities.cpp
@@ -1,6 +1,6 @@
 //Built-in vector functionalities:
 
-#include<iostream>
+/*#include<iostream>
 #include<vector>
 #include<algorithm>
 using namespace std;
@@ -11,4 +11,22 @@ int main(){
     for(int elem:v){
         cout<<elem<<" \n";
     }
-}
+}*/
+
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    vector<int> v={4,3,8,2,9};
+    
+    sort(v.begin(),v.end());          //sorting
+    for(int elem:v) cout<<elem<<" ";
+    
+    cout<<" "<<endl;
+    
+    reverse(v.begin(),v.end());       //reversing
+    for(int elem:v) cout<<elem<<" ";
+    
+}    
