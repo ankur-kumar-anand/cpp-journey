210bcb096f3794dea1769eafd449eceeab21f20a
Ankur Kumar Anand <ankur.anand2k1@gmail.com>
2026-08-19 17:06:23 +0530

    adding 2d array examples !

---
+++ 05_Arrays/sum_of_2d_array.cpp
@@ -0,0 +1,16 @@
+//sum of all elements in given 2d array:
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[][4]={{5,8,1,2},{9,9,4,4},{7,0,3,5}};
+    int sum=0;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            cout<<arr[i][j]<<" ";
+            sum=sum+arr[i][j];
+        }
+        cout<<endl;
+    }
+    cout<<"Sum: "<<sum;
+}
