7e34315703bc03bbdffa70ca4c763a670d163220
Ankur Kumar Anand <ankur.anand2k1@gmail.com>
2026-09-05 20:41:43 +0530

    adding 2D array examples

--- 06_2D_Arrays/Search_a_2Dmatrix_II_lc240.cpp
+++ 06_2D_Arrays/Search_a_2Dmatrix_II_lc240.cpp
@@ -6,6 +6,54 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
     int m = matrix.size();
     int n = matrix[0].size();
 
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+
+            if (matrix[i][j] == target) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+int main() {
+
+    vector<vector<int>> matrix = {
+        {1, 4, 7, 11, 15},
+        {2, 5, 8, 12, 19},
+        {3, 6, 9, 16, 22},
+        {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+
+    int target;
+
+    cout << "Enter target: ";
+    cin >> target;
+
+    bool result = searchMatrix(matrix, target);
+
+    if (result) {
+        cout << "true";
+    }
+    else {
+        cout << "false";
+    }
+
+    return 0;
+}
+
+//Method:2//
+/*#include <iostream>
+#include <vector>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+
     // Start from the top-right corner
     int i = 0;
     int j = n - 1;
@@ -50,4 +98,20 @@ int main() {
     }
 
     return 0;
-}
+}*/
+
+/*//Search a 2D matrix II : leetcode 240
+class Solution{
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target){
+        int m=arr.size(), n=arr[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};*/
